20a5bf5c2c5f4ba6a23b78ad7075205aa0ecc287
YOJU-e <red23457039@gmail.com>
2024-08-19 16:46:05 +0800

    update

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 uri = "mongodb+srv://red23:dm123@cluster0.egiqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Create a new client and connect to the server
-client = MongoClient(uri,tlsCAFile=ca)
+client = MongoClient(uri)
 
 # Send a ping to confirm a successful connection
 try:
