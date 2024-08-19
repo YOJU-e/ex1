225fa6c82ff9405e0f71c8628d381517a2cb5a87
YOJU-e <red23457039@gmail.com>
2024-08-19 16:57:12 +0800

    update

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+import streamlit as st
+from pymongo import MongoClient 
 
+uri = "mongodb+srv://red23:dm123@cluster0.egiqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+db = client['test']
+collection = db['students']
+
+def fetch_data():
+    students = list(collection.find())
+    return students
+
+st.title('Student Records from MongoDB')
+    
+# 데이터 불러오기
+data = fetch_data()
+
+# 데이터 표시
+if data:
+    for student in data:
+        st.write(student)
+else:
+    st.write("No data found.")
